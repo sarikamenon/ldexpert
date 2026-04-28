d03f011088903323d0033a76936a627d224292b0
Nikhil Radadiya <nikhil.radadiya@techuplabs.com>
2026-04-28 12:07:33 +0530

    Updated telescope to dev only

--- app/bootstrap/providers.php
+++ app/bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\PaymentServiceProvider::class,
+    ...(app()->environment('local') ? [App\Providers\TelescopeServiceProvider::class] : []),
 ];
