df364c78de685dbdb0f5d9a0bcafef722a769f85
Nikhil Radadiya <nikhil.radadiya@techuplabs.com>
2026-05-07 16:36:48 +0530

    Updated button label

--- app/resources/views/therapist/schedule/fullcalendar.blade.php
+++ app/resources/views/therapist/schedule/fullcalendar.blade.php
@@ -16,7 +16,7 @@
                         <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
                         </svg>
-                        Add Indirect Session
+                        Add Indirect Service
                     </a>
                     <button id="addScheduleButton" type="button" data-create-base="{{ route('therapist.schedule.create') }}"
                         class="inline-flex items-center px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary/90 text-sm font-medium"
